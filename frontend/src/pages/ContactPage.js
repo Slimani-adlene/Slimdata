ef22a4eb8489b3278ca3e3b845bac1b717a859e5
Slimani-adlene <adlene48@outlook.fr>
2025-03-16 22:45:08 +0100

    Update ContactPage.js

--- frontend/src/pages/ContactPage.js
+++ frontend/src/pages/ContactPage.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
 
 const ContactPage = () => {
   const [formData, setFormData] = useState({
